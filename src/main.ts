e36688a89f5dbe6086188e82244ec030c6a97cf3
Leonardo <oliveirabalsa2@gmail.com>
2020-12-03 18:16:33 -0300

    chore: add main configurations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('Leonardo delivery much')
+    .setDescription('Desenvolvedor Back-end')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
